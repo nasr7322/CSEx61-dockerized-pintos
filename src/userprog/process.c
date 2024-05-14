2685f902b39f31604a34e983b1f63c64b7cbf10d
AbdElRahmanOsama182 <bodyusama207@gmail.com>
2024-05-14 21:47:28 +0300

    Argument Passing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -18,7 +18,6 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "threads/synch.h"
-
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
@@ -32,6 +31,11 @@ process_execute (const char *file_name)
   char *fn_copy;
   tid_t tid;
 
+  /* ======== Argument Passing ======== */
+  char *fn;
+  tid = TID_ERROR;
+  /* ======== Argument Passing END ======== */
+
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
@@ -39,8 +43,24 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  /* ======== Argument Passing ======== */
+  fn = malloc (strlen (file_name) + 1);
+  if (!fn){
+    free (fn);
+    palloc_free_page (fn_copy);
+    return TID_ERROR;
+  }
+  memcpy (fn, file_name, strlen (file_name) + 1);
+  char *save_ptr;
+  file_name = strtok_r (fn, " ", &save_ptr);
+  /* ======== Argument Passing END ======== */
+
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  // tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  /* ======== Argument Passing ======== */
+  tid = thread_create (fn, PRI_DEFAULT, start_process, fn_copy);
+  free (fn);
+  /* ======== Argument Passing END ======== */
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
@@ -60,12 +80,66 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
 
+/* ======== Argument Passing ======== */
+  char *token, *save_ptr;
+  void *start;
+  int argc, i;
+  int *argv_off;
+  struct thread *t;
+  t = thread_current ();
+  argc = 0;
+  argv_off = malloc (32 * sizeof (int));
+  if (!argv_off)
+    goto exit;
+  size_t len = strlen (file_name) + 1;
+  argv_off[0] = 0;
+  for (token = strtok_r (file_name, " ", &save_ptr); token != NULL; token = strtok_r (NULL, " ", &save_ptr)) {
+    while (*(save_ptr) == ' ')
+      ++save_ptr;
+    argv_off[++argc] = save_ptr - file_name;
+  }
+  /* ======== Argument Passing END ======== */
+  success = load (file_name, &if_.eip, &if_.esp);
+/* ======== Argument Passing ======== */
+  if (success)
+  {
+    if_.esp -= len;
+    start = if_.esp;
+    memcpy (if_.esp, file_name, len);
+    if_.esp -= 4 - len % 4;
+    if_.esp -= 4;
+    *(int *)(if_.esp) = 0;
+    for (i = argc - 1; i >= 0; --i)
+    {
+      if_.esp -= 4;
+      *(void **)(if_.esp) = start + argv_off[i];
+    }
+    if_.esp -= 4;
+    *(char **)(if_.esp) = (if_.esp + 4);
+    if_.esp -= 4;
+    *(int *)(if_.esp) = argc;
+    if_.esp -= 4;
+    *(int *)(if_.esp) = 0;
+    intr_disable ();
+    thread_block ();
+    intr_enable ();
+  }
+  else
+    {
+      free (argv_off);
+exit:
+      intr_disable ();
+      thread_block ();
+      intr_enable ();
+      thread_exit ();
+    }  
+  free (argv_off);
+  /* ======== Argument Passing END ======== */
   /* If load failed, quit. */
   palloc_free_page (file_name);
-  if (!success) 
-    thread_exit ();
+  // if (!success) 
+  //   thread_exit ();
 
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
@@ -443,7 +517,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE-12;
+        *esp = PHYS_BASE;
       else
         palloc_free_page (kpage);
     }
